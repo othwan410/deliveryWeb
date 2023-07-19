4dd614149d55b10017a5f9b9fafbf52cbb95aafc
Kim Taejin <rlaxowls0504@gmail.com>
2023-07-20 01:18:20 +0900

    refactory: migration store.js에 call 칼럼 추가

--- migrations/20230717031411-create-store.js
+++ migrations/20230717031411-create-store.js
@@ -37,6 +37,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      call: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
