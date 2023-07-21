0b7b72663df4d33fc1419fd49d6cf8ef1da0663c
Taehwan Oh <othwan410@daum.net>
2023-07-21 12:22:37 +0900

    temp

--- migrations/20230717030321-create-dibs.js
+++ migrations/20230717030321-create-dibs.js
@@ -3,12 +3,6 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Dibs', {
-      dibs_id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
       store_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -16,6 +10,7 @@ module.exports = {
           model: 'Stores', // Stores 모델을 참조합니다.
           key: 'store_id', // Stores 모델의 store_id 참조합니다.
         },
+        primaryKey: true,
         onDelete: 'CASCADE', // 만약 Stores 모델의 store_id 삭제되면
       },
       user_id: {
@@ -25,6 +20,7 @@ module.exports = {
           model: 'Users', // Users 모델을 참조합니다.
           key: 'user_id', // Users 모델의 userId를 참조합니다.
         },
+        primaryKey: true,
         onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면
       },
       createdAt: {
